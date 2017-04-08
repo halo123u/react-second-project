ec5b97081b0d6bf4ee8359d9256c24623836f2b2
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-07 21:09:40 -0500

    add BookList redux container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {title: 'Javascript: the good Parts'},
+        {title: 'Harry potter'},
+        {title: 'the dark tower'},
+        {title: 'eloquent ruby'}
+    ]
+}
