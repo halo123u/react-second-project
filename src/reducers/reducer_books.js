4d05865f469181b07c3ae879586a3ae6b6a429c7
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-08 23:10:06 -0500

    add Action to select a book and display

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
     return [
-        {title: 'Javascript: the good Parts'},
-        {title: 'Harry potter'},
-        {title: 'the dark tower'},
-        {title: 'eloquent ruby'}
+        {title: 'Javascript: the good Parts', pages: 101},
+        {title: 'Harry potter', pages: 39},
+        {title: 'the dark tower', pages: 65},
+        {title: 'eloquent ruby', pages: 1}
     ]
 }
